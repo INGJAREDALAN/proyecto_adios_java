f5281376a8265da235eef7049ffd87bed7b0c404
INGJAREDALAN <perezpichardojaredalan2@gmail.com>
2026-05-18 09:48:11 -0600

    Metodo multiplicar

--- AdiosJava.java
+++ AdiosJava.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class AdiosJava{
 
+    public static int multiplicar(int n1, n2){
+        return n1 * n2;
+    }
+
 public static Scanner scanner = new Scanner(System.in);
 
 public static int Resultadoresta(){
@@ -37,5 +41,14 @@ public static int Resultadoresta(){
         int resultado = numero1 + numero2;
         System.out.println("La suma es: " + resultado);
 
-        
+        Persona persona1;
+        persona1 = new Persona("Jared Alan", "Perez", "Pichardo");
+
+        persona1.setedad(19);
+        persona1.setDomicilio("Avenida San Javier 64");
+        persona1.settelefono("247 133 7626");
+
+
+        System.out.println("\n" + persona1.toString());
+        scanner.close();
     }
