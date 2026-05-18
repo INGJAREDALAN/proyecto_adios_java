8909bed568d1aed069bbcc3a1f4d67a4cd676eef
INGJAREDALAN <perezpichardojaredalan2@gmail.com>
2026-05-17 22:16:52 -0600

    Programa que resta dos numeros

--- AdiosJava.java
+++ AdiosJava.java
@@ -1,6 +1,31 @@
 import java.util.Scanner; 
+
 public class AdiosJava{
+
+public static Scanner scanner = new Scanner(System.in);
+
+public static int Resultadoresta(){
+            
+    int numero_resta1, numero_resta2, resta;
+
+    System.out.println("\noperacion aritmetica -Resta-");
+
+    System.out.println("Escriba el primer numero de la resta: ");
+    numero_resta1 = scanner.nextInt();
+    System.out.println("Escriba el segundo numero de la resta: ");
+    numero_resta2 = scanner.nextInt();
+
+    resta = numero_resta1 - numero_resta2;
+    return resta;
+
+
+        } 
+
+    }
+
+
     public static void main (String[] args){
+
         System.out.println("Adios Java");
         System.out.println("Comenzaremos a usar Python");
         int numero1, numero2;
@@ -11,5 +36,6 @@ public class AdiosJava{
         numero2 = scanner.nextInt();
         int resultado = numero1 + numero2;
         System.out.println("La suma es: " + resultado);
+
+        
     }
-}
