31fdfc7ecaa27b6405d1833fbb1aa4c48391c634
INGJAREDALAN <perezpichardojaredalan2@gmail.com>
2026-05-19 10:02:15 -0600

    Division

--- AdiosJava.java
+++ AdiosJava.java
@@ -2,38 +2,39 @@ import java.util.Scanner;
 
 public class AdiosJava{
 
-    public static int multiplicar(int n1, n2){
-        return n1 * n2;
+    public static double dividir(double numero1, double numero2){
+        return numero1/numero2;
     }
 
-public static Scanner scanner = new Scanner(System.in);
-
-public static int Resultadoresta(){
-            
-    int numero_resta1, numero_resta2, resta;
-
-    System.out.println("\noperacion aritmetica -Resta-");
-
-    System.out.println("Escriba el primer numero de la resta: ");
-    numero_resta1 = scanner.nextInt();
-    System.out.println("Escriba el segundo numero de la resta: ");
-    numero_resta2 = scanner.nextInt();
-
-    resta = numero_resta1 - numero_resta2;
-    return resta;
-
-
-        } 
-
+    public static int multiplicar(int numero1, int numero2){
+        return numero1 * numero2;
     }
 
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static int Resultadoresta(){
+                
+        int numero_resta1, numero_resta2, resta;
+
+        System.out.println("\noperacion aritmetica -Resta-");
+
+        System.out.println("Escriba el primer numero de la resta: ");
+        numero_resta1 = scanner.nextInt();
+        System.out.println("Escriba el segundo numero de la resta: ");
+        numero_resta2 = scanner.nextInt();
+
+        resta = numero_resta1 - numero_resta2;
+        return resta;
+
+
+    } 
 
     public static void main (String[] args){
 
         System.out.println("Adios Java");
         System.out.println("Comenzaremos a usar Python");
         int numero1, numero2;
-        Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Escribe el numero 1: ");
         numero1 = scanner.nextInt();
         System.out.println("Escribe el numero 2: ");
@@ -41,14 +42,17 @@ public static int Resultadoresta(){
         int resultado = numero1 + numero2;
         System.out.println("La suma es: " + resultado);
 
-        Persona persona1;
-        persona1 = new Persona("Jared Alan", "Perez", "Pichardo");
+        int mult = multiplicar(numero1, numero2);
+
+        Persona1 persona1 = new Persona1("Jared", "Perez", "Pichardo");
 
         persona1.setedad(19);
-        persona1.setDomicilio("Avenida San Javier 64");
+        persona1.setdomicilio("Avenida San Javier 64");
         persona1.settelefono("247 133 7626");
 
 
         System.out.println("\n" + persona1.toString());
         scanner.close();
     }
+
+}
